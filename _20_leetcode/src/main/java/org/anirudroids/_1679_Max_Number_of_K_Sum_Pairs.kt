17025d65bd61882f7a9c0067cc6793621aad0395
Aniruddha Trikoliker <aniruddha.trikoliker@gmail.com>
2024-09-19 19:57:28 +0530

    Added content for database and sql module

--- _20_leetcode/src/main/java/org/anirudroids/_1679_Max_Number_of_K_Sum_Pairs.kt
+++ _20_leetcode/src/main/java/org/anirudroids/_1679_Max_Number_of_K_Sum_Pairs.kt
@@ -1,7 +1,54 @@
 package org.anirudroids
 
 fun main() {
-    val set = intArrayOf(1,2,3).toSet()
+    val arr = Array<IntArray>(3) { intArrayOf() }
+
+    arr[0] = intArrayOf(3,2,1)
+    arr[1] = intArrayOf(1,7,6)
+    arr[2] = intArrayOf(2,7,7)
+
+    print(equalPairs(arr))
+}
+
+fun equalPairs(grid: Array<IntArray>): Int {
+    val rowsMap = mutableMapOf<List<Int>, Int>()
+
+    for(intArray in grid) {
+        rowsMap[intArray.toList()] = (rowsMap[intArray.toList()] ?: 0) + 1
+    }
+
+    var output = 0
+    for(columnIndex in grid.first().indices) {
+        val array = IntArray(grid.size) {0}
+        for(rowIndex in grid.indices) {
+            array[rowIndex] = grid[columnIndex][rowIndex]
+        }
+
+        rowsMap[array.toList()]?.let {
+            output += it
+        }
+    }
+
+    return output
+}
+
+fun closeStrings(word1: String, word2: String): Boolean {
+    if(word1.length != word2.length) return false
+
+    val map1 = mutableMapOf<Char, Int>()
+
+    for(ch in word1.toCharArray()) {
+        map1[ch] = (map1[ch] ?: 0) + 1
+    }
+
+    val map2 = mutableMapOf<Char, Int>()
+
+    for(ch in word2.toCharArray()) {
+        if(!map1.containsKey(ch)) return false
+        map2[ch] = (map2[ch] ?: 0) + 1
+    }
+
+    return map1.values.sorted() == map2.values.sorted()
 }
 
 fun maxOperations(nums: IntArray, k: Int): Int {
