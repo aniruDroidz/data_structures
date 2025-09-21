f405a8f9abbe4ec4b34c25f77fc7e51ffc597177
Aniruddha Trikoliker <aniruddha.trikoliker@gmail.com>
2025-09-21 18:16:49 +0530

    Update Q2_Largest_Rectangle_in_Histogram.java

--- _12_stacks/src/main/java/org/anirudroids/_52_nearest_smaller_and_basics_problems/problems/mandatory/Q2_Largest_Rectangle_in_Histogram.java
+++ _12_stacks/src/main/java/org/anirudroids/_52_nearest_smaller_and_basics_problems/problems/mandatory/Q2_Largest_Rectangle_in_Histogram.java
@@ -7,7 +7,8 @@ public class Q2_Largest_Rectangle_in_Histogram {
      * Problem Description
      * Given an array of integers A.
      *
-     * A represents a histogram i.e A[i] denotes the height of the ith histogram's bar. Width of each bar is 1.
+     * A represents a histogram i.e A[i] denotes the height of the ith 
+histogram's bar. Width of each bar is 1.
      *
      * Find the area of the largest rectangle formed by the histogram.
      *
